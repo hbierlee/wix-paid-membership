0422289da00bb9b1f3decf26558c5ebba4b880fc
Henk Bierlee <bierlee.henk@gmail.com>
2018-03-09 20:24:32 +0100

    Fix redirect to subscribe page from subscribers-only (when not subscribed)

--- Public/allowSubscribersOnly.js
+++ Public/allowSubscribersOnly.js
@@ -2,7 +2,7 @@ import wixUsers from 'wix-users';
 import wixLocation from 'wix-location';
 import {userIsSubscribed} from 'backend/subscribe';
 
-export default async function allowSubscribersOnly(redirect = 'https://bierleehenk.wixsite.com/henk-bierlee') {
+export default async function allowSubscribersOnly(redirect = '/subscribe') {
 
   const userId = wixUsers.currentUser.id;
   const accessGranted = await userIsSubscribed(userId);
