ca27376b815fc7a3f3c0c37fb039b91d71059b23
Henk Bierlee <bierlee.henk@gmail.com>
2018-03-10 21:41:35 +0100

    Replace front-end redirecting for ACL router

---
+++ Backend/router.js
@@ -0,0 +1,30 @@
+import {ok, redirect, forbidden} from 'wix-router';
+import {getSubscriberByUserId} from './database';
+
+const REDIRECT_URL = 'https://bierleehenk.wixsite.com/henk-bierlee/subscribe';
+
+async function userIsSubscribed(userId) {
+  const subscriber = await getSubscriberByUserId(userId);
+  return subscriber && subscriber.isSubscribed;
+}
+
+export async function subscribers_Router(request) {
+  try {
+    const {user} = request;
+
+    if (user.role === 'Admin') {
+      return ok('Premium');
+    } else if (user.role === 'Member' && await userIsSubscribed(user.id)) {
+      return ok('Premium');
+    } else {  // 'Visitor'
+      return redirect(REDIRECT_URL); // TODO will this prompt login? probably when subscribers page is in member page (as it should be)
+    }
+  } catch (e) {
+    console.error('error in router', e);
+    return redirect(REDIRECT_URL);
+  }
+}
+
+export async function subscribers_SiteMap() {
+  return []; // TODO
+}
