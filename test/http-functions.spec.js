71dd558f14ae684993d97ab3e81af5b613419a82
Henk Bierlee <bierlee.henk@gmail.com>
2018-03-17 14:30:53 +0100

    Move test waiting on user input to beforeEach in unit test

--- test/http-functions.spec.js
+++ test/http-functions.spec.js
@@ -4,6 +4,19 @@ import {testSubscriber} from 'wix-data';
 import {handleFirstPayment, post_recurringPayment} from '../backend/http-functions';
 import {createFirstPayment, createMollieCustomer} from '../backend/mollie';
 
+async function waitOnUserInput() {
+  return await new Promise((resolve, reject) => {
+    prompt.start();
+    prompt.get(['press return once test payment is accepted with status \'paid\''], (err) => {
+      if (err) {
+        return reject(err);
+      } else {
+        return resolve();
+      }
+    });
+  });
+}
+
 
 describe('webhook', function () {
 
@@ -15,22 +28,20 @@ describe('webhook', function () {
     customer = await createMollieCustomer(testSubscriber.userId, testSubscriber.email, testSubscriber._id);
     payment = await createFirstPayment(customer.id, true);
     console.log('paymentId', payment.id);
+    console.log('wait on user accepting payment on the following URL:');
+    console.log(payment.links.paymentUrl);
+    await waitOnUserInput();
   });
 
   it('should process a payment and (if successful) subscribe the customer', function (done) {
-    console.log('waiting on accepting payment on');
-    console.log(payment.links.paymentUrl);
-    prompt.start();
-    prompt.get(['placeholder'], function (err, result) {
-      handleFirstPayment({body: {text: () => `id=${payment.id}`}})
-        .then(() => {
-          done()
-        })
-        .catch(e => {
-          console.log('e', e);
-          done(new Error(e));
-        });
-    });
+    handleFirstPayment({body: {text: () => `id=${payment.id}`}})
+      .then(() => {
+        done()
+      })
+      .catch(e => {
+        console.log('e', e);
+        done(new Error(e));
+      });
   });
 
   xit('should process a failed recurring payment and unsubscribe the customer', async function () {
